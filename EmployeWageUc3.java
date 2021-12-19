5c880746c01ee35681c4339cb6c3bf0311dd878c
Arpit <arpitmh23@gmail.com>
2021-12-19 14:17:53 +0530

    Implementing Uc3

---
+++ EmployeWageUc3.java
@@ -0,0 +1,18 @@
+class EmployeWageUc3 {
+public static void main(String[] Args) {
+	int IsFullTime = 1;
+	int IsPartTime = 2;
+	int EmployeRatePerHour = 20;
+	int EmployeWages = 0;
+	int EmployeHour = 0;
+	double EmpCheck = Math.floor(Math.random() * 10) % 3;
+	if (EmpCheck == IsFullTime)
+	   EmployeHour = 8;
+	else if (EmpCheck == IsPartTime)
+	   EmployeHour = 8;
+	else
+		EmployeHour = 0;
+    EmployeWages = EmployeHour * EmployeRatePerHour;
+	System.out.println("Employewage: " + EmployeWages );
+	}
+}
